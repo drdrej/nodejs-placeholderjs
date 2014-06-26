d97d7d3ab8e39d7697f75800513a13352329bbab
drdrej <andrej.siebert@googlemail.com>
2014-06-26 18:47:04 +0200

    refactored transformation

--- index.js
+++ index.js
@@ -10,129 +10,73 @@ var config = require( "./placeholder.json" );
 // include underscore: nice lib
 var _ = require( "underscore" );
 
-tools.query( config )
-    .select( '.images > *' )
-    .transform( function( image ){
-        var path = image.path + "/" + image.src + "." + image.type;
-        var width = image.width;
-        var height = image.height;
-        var output = image.output + "/" + image.src + "_" + width + "x" + height + "." + image.type;
+var resizeFnc = function( image, path, output ) {
+    console.log( "-- create thumbnail: " + image.path );
+    var fs = require('fs');
+    var gm = require('gm');
 
-        console.log( "-- resize image: " + path );
-        console.log( "-- resized path: " + output );
-
-        return {
-          "path"   : path,
-          "output" : output,
-          "width"  : width,
-          "height" : height,
-
-          "src"    : image
-        };
-    })
-    .transform( function( image ) {
-        console.log( "-- create thumbnail: " + image.path );
-        var fs = require('fs');
-        var gm = require('gm');
-
-        gm( image.path )
-            .resize( image.width, image.height)
-            .noProfile()
-            .write( image.output, function (err) {
-                if (!err)
-                    console.log('-- IMAGE resize done! path: ' + image.output );
-                else
-                    console.log( err );
-            });
-
-        // return original image after transformation.
-        return image;
-    })
-    .transform( function( image ) {
-        console.log( "-- PREPARE COPIES ..." );
-
-        var copies = image.src.copy;
-        if( !_.isArray(copies) ) {
-            console.log( "WARN: couldn't create copies. should be an array" );
-        }
-
-        var result = []
-
-        _.each( copies, function( name ) {
-            console.log( "-- create path for: " + name);
-
-            var copy = {
-                path: image.path,
-                width: image.src.width,
-                height: image.src.height,
-                output: (image.src.output + "/" + name + "_" + image.src.width + "x" + image.src.height + "." + image.src.type)
-            };
-
-            result.push(copy);
+    gm( path )
+        .resize( image.width, image.height)
+        .noProfile()
+        .write( output, function (err) {
+            if (!err)
+                console.log('-- IMAGE resize done! path: ' + image.output );
+            else
+                console.log( err );
         });
 
-        console.log( "-- ready!!!" );
-        return result;
-    })
-    .split()
-    .transform( function( image ) {
-        console.log( "-- create thumbnail: " + image.path );
-        var fs = require('fs');
-        var gm = require('gm');
+    // return original image after transformation.
+    return image;
+};
 
-        gm( image.path )
-            .resize( image.width, image.height)
-            .noProfile()
-            .write( image.output, function (err) {
-                if (!err)
-                    console.log('-- IMAGE resize done! path: ' + image.output );
-                else
-                    console.log( err );
-            });
+var resize = function(image) {
+    return resizeFnc(image, image.path, image.output)
+};
 
-        // return original image after transformation.
-        return image;
+var prepareBasicThumbnail = function( image ){
+    console.log( "-- TRAMSFORMATION: create img.resize.node");
+    return {
+        "$type"    : "img/resize",
+        "path"   : (image.path + "/" + image.src + "." + image.type),
+        "output" : (image.output + "/" + image.src + "_" + image.width + "x" + image.height + "." + image.type),
+        "width"  : image.width,
+        "height" : image.height,
+
+        "src"    : image
+    };
+};
+
+var prepareCopiesThumbnails = function( image ) {
+    console.log( "-- PREPARE COPIES ..." );
+
+    var copies = image.src.copy;
+    if( !_.isArray(copies) ) {
+        console.log( "WARN: couldn't create copies. should be an array" );
+    }
+
+    var result = []
+
+    _.each( copies, function( name ) {
+        console.log( "-- create path for: " + name);
+
+        var copy = {
+            path: image.path,
+            width: image.src.width,
+            height: image.src.height,
+            output: (image.src.output + "/" + name + "_" + image.src.width + "x" + image.src.height + "." + image.src.type)
+        };
+
+        result.push(copy);
     });
 
-    // TODO: split  array in elements ...
-    //.split();
+    console.log( "-- ready!!!" );
+    return result;
+}
 
-
-
-
-
-
-/*
-       "path"   : "./img",
-        "src"    : "test.png",
-        "type"   : "png",
-
-        "width"  : 100,
-        "height" : 100,
-
-        "copy" : [
-            "xyz"
-        ]
-
-*/
-// http://placehold.it/350x150
-
-// * [gulp-changed](https://www.npmjs.org/package/gulp-changed/): only resize changed images
-//```js
-//
-
-/*
-var gulp    = require('gulp');
-var imageResize = require('gulp-image-resize');
-
-var changed = require("gulp-changed");
-*/
-// .pipe(changed("dist"))
-// "src/**/*.{jpg,png}"
-/*
-gulp.task("changed", function () {
-    gulp.src( "img/test.png" )
-        .pipe(imageResize({ width : 100 }))
-        .pipe(gulp.dest("dist"));
-});
-*/
+tools.query( config )
+    .select( '.images > *' )
+    .transform( prepareBasicThumbnail )
+    .transform( resize )
+    .transform( prepareCopiesThumbnails )
+    .split()
+    .transform( resize );
